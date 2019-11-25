c67049ac780d7991af4e51e6771e805ba01628d0
Joe Gilbert <joe@gilbertcon.com>
2019-11-24 20:24:43 -0500

    adding POJOs.  Closes #2.

---
+++ src/main/java/com/gilbertcon/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.gilbertcon.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/gilbertcon/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.gilbertcon.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
