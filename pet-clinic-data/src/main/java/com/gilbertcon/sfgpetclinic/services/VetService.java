afab488190e478421fcdbcb3c1106d596dda6aaf
Gilbert, Joe (CORP) <joe.gilbert@adp.com>
2019-11-25 19:06:18 -0500

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/gilbertcon/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.gilbertcon.sfgpetclinic.services;
+
+import com.gilbertcon.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
