d9b7173159d901b318b7765862277547590c87ab
Gilbert, Joe (CORP) <joe.gilbert@adp.com>
2020-01-09 16:17:32 -0500

    Created JSON endpoint for Vets

--- pet-clinic-web/src/main/java/com/gilbertcon/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/gilbertcon/sfgpetclinic/controllers/VetController.java
@@ -1,11 +1,15 @@
 package com.gilbertcon.sfgpetclinic.controllers;
 
+import com.gilbertcon.sfgpetclinic.model.Vet;
 import com.gilbertcon.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequestMapping("/vets")
+import java.util.Set;
+
 @Controller
 public class VetController {
 
@@ -15,11 +19,16 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
+    @RequestMapping({"/vets", "/vets/", "/vets/index", "/vets/index.html"})
     public String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
